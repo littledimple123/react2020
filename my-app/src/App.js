ce5ffc83ca20ace39771f40516b86c91906ad7d6
littledimple123 <cztian1314@163.com>
2020-05-29 10:56:30 +0800

    router传参

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello world</h1>
-
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(prop) {
+    super(prop);
+    this.handleclick = this.handleclick.bind(this);
+  }
+  handleclick() {
+    this.props.history.push({
+      pathname: '/detail/index',
+      state: { id: '1' }, // 也可以用query：{id: '1'}  区别是：state是加密传输，浏览器上看不见，query不是加密传输，浏览器可以看见
+    });
+  }
+  render() {
+    return (
+      <div className='App'>
+        <h1>hello world</h1>
+        <h2 onClick={this.handleclick}>to Detail</h2>
+      </div>
+    );
+  }
 }
-
-export default App;
--- my-app/src/detail/index.js
+++ my-app/src/detail/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import '../App.css';
 export default class Detail extends React.Component {
   constructor(prop) {
     super(prop);
     this.handlerClick = this.handlerClick.bind(this);
   }
+  componentDidMount () {
+    console.log(this.props.location.state)
+  }
   handlerClick() {
     this.props.history.push({
-      pathname: '/',
+      pathname: '/'
     });
   }
   render() {
-    return <div onClick={this.handlerClick}>回到home页面</div>;
+    return (
+      <div className='App'>
+        <h1 onClick={this.handlerClick}>回到home页面</h1>
+      </div>
+    )
   }
 }
